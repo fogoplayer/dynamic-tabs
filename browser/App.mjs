21114402f5013d5cbc65366a6761b55a4f1db577
Zarin Loosli <zarinloosli@gmail.com>
2023-07-30 16:02:48 -0600

    Update imports

--- browser/App.mjs
+++ browser/App.mjs
@@ -1,8 +1,9 @@
 import "./libs/pwaupdate.js";
-import { css, LitElement } from "./libs/lit-all@2.7.6.js";
+import { html, css, LitElement } from "./libs/lit-all@2.7.6.js";
 import globalCss from "./global-styles/global.css.mjs";
 
 import Home from "./pages/Home.mjs";
+import { component } from "./libs/haunted@4.8.3.js";
 
 // Add global styles to head for resets and fonts
 const style = document.createElement("style");
@@ -16,22 +17,26 @@ export default class App extends LitElement {
 
   constructor() {
     super();
-    page.base("/browser")
+    page.base("/browser");
     this.createRoute("/", Home);
     page.start();
+
+    /** @type {HTMLElement} */
+    this.currentPage;
   }
 
   /**
    * Creates a route for the given pattern and associates it with a custom web component.
    *
    * @param {string} pattern the URL pattern to match for the route.
-   * @param {new (context: Context) => LitElement} component the component class to be instantiated when the route is activated.
+   * @param {unknown} element the component class to be instantiated when the route is activated.
    * @param {string?} title the title to display in the URL bar
    * @returns {void}
    */
-  createRoute(pattern, component, title = "Dynamic Tabs Browser") {
-    page(pattern, (context) => {
-      this.currentPage = new component(context);
+  createRoute(pattern, element, title = "Dynamic Tabs Browser") {
+    page(pattern, context => {
+      // @ts-ignore
+      this.currentPage = document.createElement("home-");
     });
   }
 
--- browser/pages/Home.mjs
+++ browser/pages/Home.mjs
@@ -1,21 +1,27 @@
-import { LitElement, html, css } from "../libs/lit-all@2.7.6.js";
+import { component, virtual, html } from "../libs/haunted@4.8.3.js";
 import globalCss from "../global-styles/global.css.mjs";
 
-export default class Home extends LitElement {
-  static get properties() {
-    return {};
-  }
+// export default class Home extends LitElement {
+//   static get properties() {
+//     return {};
+//   }
 
-  constructor() {
-    super();
-  }
+//   constructor() {
+//     super();
+//   }
 
-  render() {
-    return html`<header><h1>Dynamic Tabs Browser</h1></header>
-      <main>Welcome to my app!</main>`;
-  }
+//   render() {
+//     return html`<header><h1>Dynamic Tabs Browser</h1></header>
+//       <main>Welcome to my app!</main>`;
+//   }
 
-  static styles = [globalCss, css``];
+//   static styles = [globalCss, css``];
+// }
+
+function Home() {
+  return html`<header><h1>Dynamic Tabs Browser</h1></header>
+    <main>Welcome to my app!</main>`;
 }
 
-customElements.define("home-", Home);
+export default virtual(Home);
+customElements.define("home-", component(Home));
