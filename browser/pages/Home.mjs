ba8562bff4f9e78bd914514b106b0ae4d1655b52
Zarin Loosli <zarinloosli@gmail.com>
2023-07-30 17:55:10 -0600

    Clean up Home component

--- browser/pages/Home.mjs
+++ browser/pages/Home.mjs
@@ -1,23 +1,5 @@
-import { component, virtual, html } from "../libs/haunted@4.8.3.js";
-import globalCss from "../global-styles/global.css.mjs";
+import { html } from "../libs/haunted@4.8.3.js";
 import defineHaunted from "../services/haunted-define.mjs";
-
-// export default class Home extends LitElement {
-//   static get properties() {
-//     return {};
-//   }
-
-//   constructor() {
-//     super();
-//   }
-
-//   render() {
-//     return html`<header><h1>Dynamic Tabs Browser</h1></header>
-//       <main>Welcome to my app!</main>`;
-//   }
-
-//   static styles = [globalCss, css``];
-// }
 
 function Home() {
   return html`<header><h1>Dynamic Tabs Browser</h1></header>
