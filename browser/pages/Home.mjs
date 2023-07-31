62817ed42bffde163501dbc0af5349d16ab5d339
Zarin Loosli <zarinloosli@gmail.com>
2023-07-30 19:43:22 -0600

    Create component system

--- browser/pages/Home.mjs
+++ browser/pages/Home.mjs
@@ -1,9 +1,57 @@
-import { html } from "../libs/haunted@4.8.3.js";
+/** @typedef {import("../libs/lit-all@2.7.6.js").TemplateResult} TemplateResult */
+import { html, useState } from "../libs/haunted@4.8.3.js";
 import defineHaunted from "../services/haunted-define.mjs";
 
 function Home() {
-  return html`<header><h1>Dynamic Tabs Browser</h1></header>
-    <main>Welcome to my app!</main>`;
+  const [layers, setLayers] = useState(/** @type {string[]} */ ([]));
+  setLayers(["nav", "section", "div", "main"]);
+
+  return html`
+    <div class="layer">
+      <nav class="left">Nav</nav>
+      <div class="layer">
+        <section class="right">section</section>
+        <div class="layer">
+          <div class="top">
+            div
+            <div class="layer">
+              <footer class="bottom">Footer</footer>
+              <main>
+                Main content
+                <iframe src="" frameborder="0"></iframe>
+              </main>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <style>
+      .layer {
+        display: flex;
+        flex-wrap: nowrap;
+      }
+
+      .layer > * {
+        outline: 1px solid red;
+      }
+
+      .layer:has(> .left) {
+        flex-direction: row;
+      }
+
+      .layer:has(> .right) {
+        flex-direction: row-reverse;
+      }
+
+      .layer:has(> .top) {
+        flex-direction: column;
+      }
+
+      .layer :has(> .bottom) {
+        flex-direction: column-reverse;
+      }
+    </style>
+  `;
 }
 
 export default defineHaunted("home-", Home);
