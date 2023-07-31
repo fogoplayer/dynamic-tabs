a443ccefe195ed34dcf46fdf824dd1af37c39a86
Zarin Loosli <zarinloosli@gmail.com>
2023-07-30 22:19:13 -0600

    Add panels

--- browser/pages/Home.mjs
+++ browser/pages/Home.mjs
@@ -1,21 +1,97 @@
 import { LitElement, html, css } from "../libs/lit-all@2.7.6.js";
 import globalCss from "../global-styles/global.css.mjs";
+import "https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js";
+import "https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js";
+import userSettings from "../services/user-settings.mjs";
 
 export default class Home extends LitElement {
-  static get properties() {
-    return {};
-  }
-
-  constructor() {
-    super();
-  }
-
   render() {
-    return html`<header><h1>Dynamic Tabs Browser</h1></header>
-      <main>Welcome to my app!</main>`;
+    return html`<div class="layer">
+      <nav class="${userSettings.panels[0].position}">Nav</nav>
+      <div class="layer">
+        <section class="${userSettings.panels[1].position}">section</section>
+        <div class="layer">
+          <div class="${userSettings.panels[2].position}">div</div>
+          <div class="layer">
+            <footer class="${userSettings.panels[3].position}">Footer</footer>
+            <main>
+              Main content
+              <!-- <iframe src="" frameborder="0"></iframe> -->
+              <ion-list>
+                ${userSettings.panels.map(
+                  (panel, i) =>
+                    html`<ion-item>
+                      <ion-select
+                        label="${panel.element}"
+                        label-placement="fixed"
+                        interface="popover"
+                        value="${panel.position}"
+                        @ionChange=${async e => {
+                          console.log(e);
+                          userSettings.panels[i].position = e.detail.value;
+                          await this.requestUpdate();
+                          console.log(userSettings);
+                        }}
+                      >
+                        ${this.dropdownOptions()}
+                      </ion-select>
+                    </ion-item>`
+                )}
+              </ion-list>
+            </main>
+          </div>
+        </div>
+      </div>
+    </div>`;
   }
 
-  static styles = [globalCss, css``];
+  dropdownOptions() {
+    return html`<ion-select-option value="top">Top</ion-select-option>
+      <ion-select-option value="bottom">Bottom</ion-select-option>
+      <ion-select-option value="left">Left</ion-select-option>
+      <ion-select-option value="right">Right</ion-select-option>
+      <ion-select-option value="none">None</ion-select-option>`;
+  }
+
+  static styles = [
+    globalCss,
+    css`
+      .layer {
+        display: flex;
+        flex-wrap: nowrap;
+      }
+
+      .layer > * {
+        outline: 1px solid red;
+        flex: 1;
+      }
+
+      .layer > :first-child {
+        flex: 0;
+      }
+
+      .layer:has(> .left) {
+        flex-direction: row;
+      }
+
+      .layer:has(> .right) {
+        flex-direction: row-reverse;
+      }
+
+      .layer:has(> .top) {
+        flex-direction: column;
+      }
+
+      .layer :has(> .bottom) {
+        flex-direction: column-reverse;
+      }
+
+      ion-list {
+        max-width: var(--limited-width);
+        margin-block: auto;
+      }
+    `,
+  ];
 }
 
 customElements.define("home-", Home);
---
+++ browser/services/user-settings.mjs
@@ -0,0 +1,9 @@
+export default {
+  /** @type {{element: string, position: "top" | "bottom" | "left" | "right"}[]} */
+  panels: [
+    { element: "nav", position: "top" },
+    { element: "section", position: "top" },
+    { element: "div", position: "top" },
+    { element: "footer", position: "top" },
+  ],
+};
