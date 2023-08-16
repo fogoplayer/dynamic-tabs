ce475ca9538c00d63ced61c8fd7a2ece94b04651
Zarin Loosli <zarinloosli@gmail.com>
2023-08-15 19:57:01 -0600

    Track state in Browser

--- browser/pages/Browser.mjs
+++ browser/pages/Browser.mjs
@@ -1,22 +1,63 @@
 /** @typedef {import("../services/daos/WidgetDAO.mjs").WidgetSettingSchema} WidgetSettingSchema */
 /** @typedef {import("../components/WidgetLayer.mjs").default} WidgetLayer */
+/** @typedef {import("../libs/firebase/9.7.0/firebase-firestore.js").DocumentReference} DocumentReference */
+/** @typedef {import("../services/daos/ProfileDAO.mjs").ProfileData} ProfileData */
+/** @typedef {import("../services/daos/SessionDAO.mjs").SessionData} SessionData */
+/** @typedef {import("../services/daos/WindowDAO.mjs").WindowData} WindowData */
+/** @typedef {import("../services/daos/TabsDAO.mjs").TabData} TabData */
 import { LitElement, css } from "../libs/lit-all@2.7.6.js";
 import globalCss from "../global-styles/global.css.mjs";
 import "../components/IframesWidget.mjs";
 import "../components/SessionsWidget.mjs";
 import "../components/TabsWidget.mjs";
-
 import WebView from "../components/Webview.mjs";
-import { watchUserSettings } from "../services/daos/UserDAO.mjs";
+import { watchUserData, watchUserSettings } from "../services/daos/UserDAO.mjs";
+
+/**
+ * @typedef {{
+ *  profile: ProfileData | null;
+ *  session: SessionData | null;
+ *  window: WindowData | null;
+ *  tab: number | null;
+ * }} BrowserState
+ */
+
+const BROWSER_STATE_LOCALSTORAGE_KEY = "browser-state";
 
 export default class Browser extends LitElement {
   static properties = {
     widgets: { type: Object, state: true },
+    state: { type: Object, state: true },
   };
 
   constructor() {
     super();
     watchUserSettings(({ widgets }) => (this.widgets = widgets));
+    watchUserData(({ profiles }) => {
+      if (
+        this.state.profile === null ||
+        this.state.session === null ||
+        this.state.window === null ||
+        this.state.tab === null
+      ) {
+        /** @type {BrowserState} */
+        const newState = this.state;
+        if (newState.profile === null) newState.profile = profiles[0];
+        if (newState.session === null) newState.session = newState.profile.sessions[0];
+        if (newState.window === null) newState.window = newState.session.windows[0];
+        if (newState.tab === null) newState.tab = 0;
+        this.state = { ...newState };
+      }
+    });
+
+    /** @type {WidgetLayer[]} */
+    this.widgetEls = [];
+
+    /** @type {BrowserState} */
+    this.state = JSON.parse(
+      localStorage.getItem(BROWSER_STATE_LOCALSTORAGE_KEY) ||
+        `{ "profile": null, "session": null, "window": null, "tab": null}`
+    );
   }
 
   render() {
@@ -51,6 +92,16 @@ export default class Browser extends LitElement {
     currentElement.innerHTML += new WebView().outerHTML;
 
     return renderRoot;
+  }
+
+  /**
+   *
+   * @param {keyof BrowserState} key
+   * @param {any} value
+   */
+  setBrowserState(key, value) {
+    this.state[key] = value;
+    localStorage.setItem(BROWSER_STATE_LOCALSTORAGE_KEY, JSON.stringify(this.state));
   }
 
   static styles = [
