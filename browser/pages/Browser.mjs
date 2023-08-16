edb6a943a77a5ded2148ce8b8e5fdddc1a610253
Zarin Loosli <zarinloosli@gmail.com>
2023-08-15 18:13:16 -0600

    Try to incorporate user data into browser

--- browser/pages/Browser.mjs
+++ browser/pages/Browser.mjs
@@ -1,5 +1,6 @@
 /** @typedef {import("../services/daos/WidgetDAO.mjs").WidgetSettingSchema} WidgetSettingSchema */
 /** @typedef {import("../components/WidgetLayer.mjs").default} WidgetLayer */
+/** @typedef {import("../libs/firebase/9.7.0/firebase-firestore.js").DocumentReference} DocumentReference */
 import { LitElement, css } from "../libs/lit-all@2.7.6.js";
 import globalCss from "../global-styles/global.css.mjs";
 import "../components/IframesWidget.mjs";
@@ -7,16 +8,46 @@ import "../components/SessionsWidget.mjs";
 import "../components/TabsWidget.mjs";
 
 import WebView from "../components/Webview.mjs";
-import { watchUserSettings } from "../services/daos/UserDAO.mjs";
+import { watchUserData, watchUserSettings } from "../services/daos/UserDAO.mjs";
+
+/**
+ * @typedef {{
+ *  profile: DocumentReference | null;
+ *  session: DocumentReference | null;
+ *  window: DocumentReference | null;
+ *  tab: DocumentReference | null;
+ * }} BrowserState
+ */
 
 export default class Browser extends LitElement {
   static properties = {
-    widgets: { type: Object, state: true },
+    widgets: { type: Array, state: true },
+    widgetEls: { type: Array, state: true },
   };
 
   constructor() {
     super();
     watchUserSettings(({ widgets }) => (this.widgets = widgets));
+    watchUserData(({ profiles }) => {
+      console.log(profiles);
+    });
+
+    /** @type {WidgetLayer[]} */
+    this.widgetEls = [];
+
+    /** @type {BrowserState} */
+    this.state = JSON.parse(
+      localStorage.getItem("browser-state") || `{ "profile": null, "session": null, "window": null, "tab": null}`
+    );
+  }
+
+  /**
+   * @param {UpdatedDiff} diff
+   */
+  update(diff) {
+    if (diff.has("widgetEls")) {
+      this.widgetEls.forEach((widgetEl, i) => (widgetEl.settingsRef = this.widgets[i].ref));
+    }
   }
 
   render() {
@@ -25,7 +56,7 @@ export default class Browser extends LitElement {
     // Render layers dynamically in order
     const [topLayer, ...others] = this.widgets;
     let renderRoot = /** @type {WidgetLayer} */ (document.createElement(topLayer.tag));
-    renderRoot.settingsRef = topLayer.ref;
+    this.widgetEls.push(renderRoot);
 
     // collapse array into a bunch of nested layer elements
     const currentElement = others.reduce(
@@ -38,10 +69,7 @@ export default class Browser extends LitElement {
        */
       (prevLayer, currLayer, index) => {
         prevLayer.innerHTML += document.createElement(currLayer.tag).outerHTML;
-
-        /** @type {WidgetLayer} */
-        (prevLayer.lastElementChild).settingsRef = currLayer.ref;
-
+        this.widgetEls.push(/** @type {WidgetLayer} */ (prevLayer.lastElementChild));
         return /** @type {WidgetLayer} */ (prevLayer.lastElementChild);
       },
       renderRoot
@@ -49,7 +77,7 @@ export default class Browser extends LitElement {
 
     // Render other contents
     currentElement.innerHTML += new WebView().outerHTML;
-
+    debugger;
     return renderRoot;
   }
 
